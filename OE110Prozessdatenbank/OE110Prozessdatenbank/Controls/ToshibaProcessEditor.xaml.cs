9b6a6be87efe7d8fa4dc0a7d1820109b6bc32f66
kilian.hammesfahr@gmx.de <kilian.hammesfahr@gmx.de>
2014-06-26 13:39:31 +0200

    kati

--- OE110Prozessdatenbank/OE110Prozessdatenbank/Controls/ToshibaProcessEditor.xaml.cs
+++ OE110Prozessdatenbank/OE110Prozessdatenbank/Controls/ToshibaProcessEditor.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Collections.ObjectModel;
+using System.Reflection;
 
 namespace OE110Prozessdatenbank.Controls
 {
@@ -22,11 +23,15 @@ namespace OE110Prozessdatenbank.Controls
     {
         ViewModels.PToshibaImportVM m_vm;
         private List<string> m_countSelection;
+        private List<MenuItem> m_items;
+        int index = 0;
         public ToshibaProcessEditor(ViewModels.PToshibaImportVM vm, int index)
         {
             InitializeComponent();
             m_countSelection = new List<string>();
+            m_items = new List<MenuItem>();
             generateCountSelector(vm.Processes.Count - index);
+            this.index = index;
             m_vm = vm;
             DataContext = this;// vm.Processes[index];
             //cb1.DataContext = this;
@@ -36,6 +41,13 @@ namespace OE110Prozessdatenbank.Controls
 
         public ObservableCollection<string> CountSelectionArray
         { get { return new ObservableCollection<string>(m_countSelection); } }
+
+        public ObservableCollection<MenuItem> Items
+        { get { return new ObservableCollection<MenuItem>(m_items); } }
+
+        public ViewModels.PToshibaVM VM
+        { get { return m_vm.Processes[index]; } }
+
 
         public int Selection
         {
@@ -60,11 +72,37 @@ namespace OE110Prozessdatenbank.Controls
             for (int i=1; i<count; i++)
             {
                 if (i == 1)
+                {
                     m_countSelection.Add("Übernehmen für den nächsten");
+                    MenuItem item = new MenuItem();
+                    item.Header = "den nächsten";
+                    item.Name = "mi_"+i.ToString(); 
+                    m_items.Add(item);
+                }
                 else
+                {
                     m_countSelection.Add("Übernehmen für die nächsten " + i);
+                    MenuItem item = new MenuItem();
+                    item.Header = "die nächsten " + i;
+                    item.Name = "mi_" + i.ToString();
+                    item.Click += item_Click;
+                    m_items.Add(item);
+                }
             }
         }
+
+        void item_Click(object sender, RoutedEventArgs e)
+        {
+            MenuItem mnu = sender as MenuItem;
+            string name = mnu.Name;
+
+
+            object t = (ContextMenu)mnu.Parent;
+
+            test = Convert.ToInt32(name.Remove(0, 3));
+        }
+
+        int test;
 
         private void cmb_analyse_Click(object sender, RoutedEventArgs e)
         {
@@ -108,5 +146,58 @@ namespace OE110Prozessdatenbank.Controls
                 }
             }
         }
+
+       
+
+        private void take_all(object sender, RoutedEventArgs e)
+        {
+            MenuItem mnu = sender as MenuItem;
+            TextBox sp = null;
+            if (mnu != null)
+            {
+                sp = ((ContextMenu)mnu.Parent).PlacementTarget as TextBox;
+            }
+        }
+
+        private void takespec(object sender, RoutedEventArgs e)
+        {
+            MenuItem mnu = sender as MenuItem;
+            TextBox sp = null;
+            if (mnu != null)
+            {
+                //MenuItem bb;
+                //bb = ((MenuItem)mnu.Parent) as MenuItem;
+                sp = ((ContextMenu)mnu.Parent).PlacementTarget as TextBox;
+
+                Binding myBinding = BindingOperations.GetBinding(sp, TextBox.TextProperty);
+
+                string dd = myBinding.Path.Path.Remove(0, 3);
+                
+                //PropertyInfo property = typeof(ViewModels.PToshibaVM).GetProperty(myBinding.ElementName);
+
+                System.Reflection.PropertyInfo prop = typeof(ViewModels.PToshibaVM).GetProperty(dd);
+
+                
+
+                
+                
+                for (int i = index + 1; i < index + test+1; i++)
+                {
+                    //object value = prop.GetValue(m_vm.Processes[i], null);
+
+                    prop.SetValue(m_vm.Processes[i], Convert.ToDouble(sp.Text.Replace('.', ',')), null);
+                    
+                    //m_vm.Processes[i].P1 = Convert.ToDouble(sp.Text.Replace('.',','));
+                }
+
+                
+
+                //property.SetValue(Convert.ToDouble(sp.Text.Replace('.',',')), "...", null);
+
+            }
+
+            //MessageBox.Show(" Textbox:" + sp.Name);
+        }
+
     }
 }
