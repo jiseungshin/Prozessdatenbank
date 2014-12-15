85f11a84a2ce509351a65a287a922680272580b1
kilian.hammesfahr@gmx.de <kilian.hammesfahr@gmx.de>
2014-12-15 21:42:09 +0100

    Toshiba anzeige verändert und Suchleiste gesperrt

--- OE110Prozessdatenbank/PDCore/Database/Queries.cs
+++ OE110Prozessdatenbank/PDCore/Database/Queries.cs
@@ -468,33 +468,33 @@ namespace PDCore.Database
 
         public static string QueryProcessedToshiba
         {
-            get
-            {
-                return "SELECT " +
+            //get
+            //{
+            //    return "SELECT " +
 
-                "`Exp_Toshiba`.`Exp_Toshiba_ID` AS `Exp_Toshiba_ID`," +
-                "`Exp_Toshiba`.`Date` AS `Date`," +
-                "`wpu`.`Label` AS `UpperWPLabel`," +
-                "`wpl`.`Label` AS `LowerWPLabel`," +
-                "`Exp_Toshiba`.`GlassName` AS `LensName`," +
-                "`Glasses`.`Glass_Name` AS `GlassName`," +
-                "`User`.`Token` AS `Token`," +
-                "`Quality`.`Glass_PV_Category` AS `PV`," +
-                "`UpperCoatingLayer`.`Layer` AS `UpperProtective`," +
-                "`LowerCoatingLayer`.`Layer` AS `LowerProtective`" +
-                    " from " +
-                        " (((((((((((`Exp_Toshiba`" +
-                        " join `Workpieces` `wpu` ON ((`Exp_Toshiba`.`UpperWP_ID` = `wpu`.`Workpiece_ID`)))" +
-                        " join `Workpieces` `wpl` ON ((`Exp_Toshiba`.`LowerWP_ID` = `wpl`.`Workpiece_ID`)))" +
-                        " join `Coating_CemeCon` `upperCoating` ON ((`Exp_Toshiba`.`UpperReference` = `upperCoating`.`Reference`)))" +
-                        " join `Coating_CemeCon` `lowerCoating` ON ((`Exp_Toshiba`.`LowerReference` = `lowerCoating`.`Reference`)))" +
-                        " join `Coating_CemeConProcesses` `UpperCoatingProcess` ON ((`upperCoating`.`CoatingProcess_ID` = `UpperCoatingProcess`.`Coating_CemeConProcesses_ID`)))" +
-                        " join `Coating_Layers` `UpperCoatingLayer` ON ((`UpperCoatingProcess`.`ProtectiveLayer` = `UpperCoatingLayer`.`Coating_Layer_ID`)))" +
-                        " join `Coating_CemeConProcesses` `LowerCoatingProcess` ON ((`lowerCoating`.`CoatingProcess_ID` = `LowerCoatingProcess`.`Coating_CemeConProcesses_ID`)))" +
-                        " join `Coating_Layers` `LowerCoatingLayer` ON ((`LowerCoatingProcess`.`ProtectiveLayer` = `LowerCoatingLayer`.`Coating_Layer_ID`)))" +
-                        " join `Glasses` ON ((`Exp_Toshiba`.`Glass_ID` = `Glasses`.`Glass_ID`)))" +
-                        " join `User` ON ((`Exp_Toshiba`.`User_ID` = `User`.`User_ID`)))" +
-                        " join `ProcessQualityParameters` `Quality` ON ((`Exp_Toshiba`.`Exp_Toshiba_ID` = `Quality`.`Process_ID`)))";
+            //    "`Exp_Toshiba`.`Exp_Toshiba_ID` AS `Exp_Toshiba_ID`," +
+            //    "`Exp_Toshiba`.`Date` AS `Date`," +
+            //    "`wpu`.`Label` AS `UpperWPLabel`," +
+            //    "`wpl`.`Label` AS `LowerWPLabel`," +
+            //    "`Exp_Toshiba`.`GlassName` AS `LensName`," +
+            //    "`Glasses`.`Glass_Name` AS `GlassName`," +
+            //    "`User`.`Token` AS `Token`," +
+            //    "`Quality`.`Glass_PV_Category` AS `PV`," +
+            //    "`UpperCoatingLayer`.`Layer` AS `UpperProtective`," +
+            //    "`LowerCoatingLayer`.`Layer` AS `LowerProtective`" +
+            //        " from " +
+            //            " (((((((((((`Exp_Toshiba`" +
+            //            " join `Workpieces` `wpu` ON ((`Exp_Toshiba`.`UpperWP_ID` = `wpu`.`Workpiece_ID`)))" +
+            //            " join `Workpieces` `wpl` ON ((`Exp_Toshiba`.`LowerWP_ID` = `wpl`.`Workpiece_ID`)))" +
+            //            " join `Coating_CemeCon` `upperCoating` ON ((`Exp_Toshiba`.`UpperReference` = `upperCoating`.`Reference`)))" +
+            //            " join `Coating_CemeCon` `lowerCoating` ON ((`Exp_Toshiba`.`LowerReference` = `lowerCoating`.`Reference`)))" +
+            //            " join `Coating_CemeConProcesses` `UpperCoatingProcess` ON ((`upperCoating`.`CoatingProcess_ID` = `UpperCoatingProcess`.`Coating_CemeConProcesses_ID`)))" +
+            //            " join `Coating_Layers` `UpperCoatingLayer` ON ((`UpperCoatingProcess`.`ProtectiveLayer` = `UpperCoatingLayer`.`Coating_Layer_ID`)))" +
+            //            " join `Coating_CemeConProcesses` `LowerCoatingProcess` ON ((`lowerCoating`.`CoatingProcess_ID` = `LowerCoatingProcess`.`Coating_CemeConProcesses_ID`)))" +
+            //            " join `Coating_Layers` `LowerCoatingLayer` ON ((`LowerCoatingProcess`.`ProtectiveLayer` = `LowerCoatingLayer`.`Coating_Layer_ID`)))" +
+            //            " join `Glasses` ON ((`Exp_Toshiba`.`Glass_ID` = `Glasses`.`Glass_ID`)))" +
+            //            " join `User` ON ((`Exp_Toshiba`.`User_ID` = `User`.`User_ID`)))" +
+            //            " join `ProcessQualityParameters` `Quality` ON ((`Exp_Toshiba`.`Exp_Toshiba_ID` = `Quality`.`Process_ID`)))";
 
                 //return "SELECT " +
 
@@ -523,54 +523,53 @@ namespace PDCore.Database
                 
                 
                 //return "SELECT * from " + DBExpToshibaView.View;
-            }
+            //}
 
-            //get
-            //{
-            //    string query = "SELECT " + DBExpToshiba.Table + ".*," +
-            //                                DBProcessQuality.Table + ".*," +
-            //                                DBGlasses.Table + ".*," +
-            //                                DBUser.Table + ".*, uwp." + DBWorkpieces.Label + " AS Upper, lwp." + DBWorkpieces.Label + " AS Lower " +
-            //                                ".*, uwp." + DBWorkpieces.Label + " AS Upper, lwp." + DBWorkpieces.Label + " AS Lower " +
+            get
+            {
+                string query = "SELECT " + DBExpToshiba.Table + ".*," +
+                                            DBProcessQuality.Table + ".*," +
+                                            DBGlasses.Table + ".*," +
+                                            DBUser.Table + ".*, uwp." + DBWorkpieces.Label + " AS Upper, lwp." + DBWorkpieces.Label + " AS Lower " +
 
-            //                                "FROM " + DBExpToshiba.Table +
+                                            "FROM " + DBExpToshiba.Table +
 
-            //                                  //join ProcessQuality
-            //                                  " LEFT JOIN " + DBProcessQuality.Table +
-            //                                  " On " + DBProcessQuality.Table + "." + DBProcessQuality.PID +
-            //                                  "=" + DBExpToshiba.Table + "." + DBExpToshiba.ID +
+                                              //join ProcessQuality
+                                              " LEFT JOIN " + DBProcessQuality.Table +
+                                              " On " + DBProcessQuality.Table + "." + DBProcessQuality.PID +
+                                              "=" + DBExpToshiba.Table + "." + DBExpToshiba.ID +
 
-            //                                  ////join References
-            //        //" LEFT JOIN " + DBProcessReferences.Table +
-            //        //" On " + DBProcessReferences.Table + "." + DBProcessReferences.RefNumber +
-            //        //"=" + DBProcessReferenceRelation.Table + "." + DBProcessReferenceRelation.RefNumber +
+                                              ////join References
+                    //" LEFT JOIN " + DBProcessReferences.Table +
+                    //" On " + DBProcessReferences.Table + "." + DBProcessReferences.RefNumber +
+                    //"=" + DBProcessReferenceRelation.Table + "." + DBProcessReferenceRelation.RefNumber +
 
-            //                                  ////join Workpiece
-            //        //" LEFT JOIN " + DBWorkpieces.Table +
-            //        //" On " + DBWorkpieces.Table + "." + DBWorkpieces.ID +
-            //        //"=" + DBProcessReferences.Table + "." + DBProcessReferences.WorkpiceID +
+                                              ////join Workpiece
+                    //" LEFT JOIN " + DBWorkpieces.Table +
+                    //" On " + DBWorkpieces.Table + "." + DBWorkpieces.ID +
+                    //"=" + DBProcessReferences.Table + "." + DBProcessReferences.WorkpiceID +
 
-            //                                    //join Glasses
-            //                                  " LEFT JOIN " + DBGlasses.Table +
-            //                                  " On " + DBGlasses.Table + "." + DBGlasses.ID +
-            //                                  "=" + DBExpToshiba.Table + "." + DBExpToshiba.GlassID +
+                                                //join Glasses
+                                              " LEFT JOIN " + DBGlasses.Table +
+                                              " On " + DBGlasses.Table + "." + DBGlasses.ID +
+                                              "=" + DBExpToshiba.Table + "." + DBExpToshiba.GlassID +
 
-            //                                   //join User
-            //                                  " LEFT JOIN " + DBUser.Table +
-            //                                  " On " + DBUser.Table + "." + DBUser.ID +
-            //                                  "=" + DBExpToshiba.Table + "." + DBExpToshiba.UserID +
+                                               //join User
+                                              " LEFT JOIN " + DBUser.Table +
+                                              " On " + DBUser.Table + "." + DBUser.ID +
+                                              "=" + DBExpToshiba.Table + "." + DBExpToshiba.UserID +
 
-            //                                   //join Workpiece
-            //                                  " LEFT JOIN " + DBWorkpieces.Table + " AS uwp " +
-            //                                  " On " + DBExpToshiba.Table + "." + DBExpToshiba.UpperWPID +
-            //                                  "=uwp." + DBWorkpieces.ID +
+                                               //join Workpiece
+                                              " LEFT JOIN " + DBWorkpieces.Table + " AS uwp " +
+                                              " On " + DBExpToshiba.Table + "." + DBExpToshiba.UpperWPID +
+                                              "=uwp." + DBWorkpieces.ID +
 
-            //                                  " LEFT JOIN " + DBWorkpieces.Table + " AS lwp " +
-            //                                  " On " + DBExpToshiba.Table + "." + DBExpToshiba.LowerWPID +
-            //                                  "=lwp." + DBWorkpieces.ID
+                                              " LEFT JOIN " + DBWorkpieces.Table + " AS lwp " +
+                                              " On " + DBExpToshiba.Table + "." + DBExpToshiba.LowerWPID +
+                                              "=lwp." + DBWorkpieces.ID
 
-            //                                 ;// +
-            //    return query;
+                                             ;// +
+                return query;
             //    ////join project
             //    //" LEFT JOIN " + DBProjects.Table +
             //    //" On " + DBProjects.Table + "." + DBProjects.ID +
@@ -581,7 +580,7 @@ namespace PDCore.Database
             //    //" On " + DBIssues.Table + "." + DBIssues.ID +
             //    //"=" + DBProcessReferences.Table + "." + DBProcessReferences.IssueID;//+
             //    //" WHERE " + DBProcessReferences.Table + "." + DBProcessReferences.Status + "='processed'";
-            //}
+            }
         }
         public static string QueryPostProcessing
         {
